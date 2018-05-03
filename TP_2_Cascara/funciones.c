cf3385a64d6e327faf912828b03f630590c0d49a
palomomatias <36156789+palomomatias@users.noreply.github.com>
2018-05-03 00:52:00 -0300

    Add files via upload

---
+++ TP_2_Cascara/funciones.c
@@ -0,0 +1,63 @@
+#include "funciones.h"
+int obtenerEspacioLibre(EPersona lista[], TAM)
+{
+    int i;
+    int index=-1;
+    for (i=0;i<TAM;i++)
+    {
+        if (lista[i].dni==0)
+        {
+          index=i;
+          break;
+        }
+    }
+
+    return index;
+}
+
+void inicializarEstado(EPersona estados[], int TAM)
+{
+    int i;
+
+    for(i=0; i<TAM; i++)
+    {
+        estados[i].estado = 0;
+    }
+}
+
+
+
+int buscarPorDni(EPersona lista[], int dni,int TAM)
+{
+    int i;
+    printf("ingrese el dni que quiere buscar");
+    scanf("%d",dni);
+    for(i=0,i<TAM;i++)
+    {
+        if(dni==lista[i].dni)
+        {
+            printf("DNI: %d NOMBRE : %s EDAD: %d",lista[i].dni,lista[i].nombre,lista[i].edad);
+        }
+        else
+        {
+            printf("error no existe ese dni reingrese");
+        }
+    }
+
+
+
+}
+
+void mostrarPersonas (EPersona mostrar [],int tam)
+{
+    int i;
+    for(i=0; i<tam; i++)
+            {
+                if(mostrar[i].estado!=0)
+                {
+                    printf("DNI:%d EDAD:%d NOMBRE: %s "mostrar[i].dni,mostrar[i].edad,mostrar[i].nombre);
+
+                }
+            }
+
+}
